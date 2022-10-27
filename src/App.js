a2adfd28727e814a5ca5a95c4c88d73d988fe932
Grace <gracenyangayi@gmail.com>
2022-10-27 13:52:45 +0300

    fetch products and stores

--- src/App.js
+++ src/App.js
@@ -8,14 +8,40 @@ import Products from './components/products/Products'
 import NavBar from './components/NavBar'
 import Page404 from './Page404';
 import ProductForm from './components/products/ProductForm';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 
 function App() {
+
+  const [product, setProduct] = useState([])
+  const [store, setStore] = useState([])
+
+
+  function getProduct(){
+    fetch("http://localhost:3000/products")
+    .then(res => res.json())
+    .then(product => setProduct(product))
+  }
+
+  useEffect(() => {
+    getProduct();
+  }, [])
+
+  function getStore(){
+    fetch("http://localhost:3000/stores")
+    .then(res => res.json())
+    .then(store => setStore(store))
+  }
+  useEffect(() => {
+    getStore();
+  }, []);
   return (
     <>
       <header>
         <NavBar />
       </header>
+     
       
       <Routes>
         <Route path='/' element={ <Home /> } />
